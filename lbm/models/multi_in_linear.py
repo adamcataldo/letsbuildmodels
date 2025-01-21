48a8551ba60e283681e595eaabc79fd44fc32722
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-01-20 21:30:16 -0800

    Added multi-input linear model

--- lbm/models/multi_in_linear.py
+++ lbm/models/multi_in_linear.py
@@ -1,13 +1,14 @@
-from lbm.models.normalizer import Normalizer
+from lbm.models import Normalizer
+from torch import nn
 
 class MultiInLinear(nn.Module):
     def __init__(self, in_features, means, stds):
         super(MultiInLinear, self).__init__()
         self.normalizer = Normalizer(means, stds)
         self.in_features = in_features
-        self.out_features = out_features
-        self.linear = nn.Linear(in_features, out_features)
+        self.linear = nn.Linear(in_features, 1)
 
     def forward(self, x):
         normalized = self.normalizer(x)
         return self.linear(normalized)
+        
