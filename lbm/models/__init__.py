4cfe9ab2364ac1869f6a802fb2188d740705031a
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-01-24 12:39:09 -0800

    Added dense linear model

--- lbm/models/__init__.py
+++ lbm/models/__init__.py
@@ -1,8 +1,10 @@
+from .linear import Linear
 from .multi_in_linear import MultiInLinear
 from .normalizer import Normalizer
 from .simple_linear import SimpleLinear
 
 __all__ = [
+    'Linear',
     'MultiInLinear', 
     'Normalizer',
     'SimpleLinear',
