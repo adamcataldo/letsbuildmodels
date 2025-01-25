276f671e435bf0cebd6979598e37524f3761ace5
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-01-24 21:50:12 -0800

    Added Logistic regression

--- lbm/models/__init__.py
+++ lbm/models/__init__.py
@@ -1,10 +1,12 @@
 from .linear import Linear
+from .logistic import Logistic
 from .multi_in_linear import MultiInLinear
 from .normalizer import Normalizer
 from .simple_linear import SimpleLinear
 
 __all__ = [
     'Linear',
+    'Logistic',
     'MultiInLinear', 
     'Normalizer',
     'SimpleLinear',
