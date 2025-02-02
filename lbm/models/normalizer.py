f625febdb6323a3b6614603a086add8661e21fd5
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-02-01 22:19:11 -0800

    MLP Classifier

--- lbm/models/normalizer.py
+++ lbm/models/normalizer.py
@@ -2,9 +2,11 @@ import torch.nn as nn
 
 class Normalizer(nn.Module):
     def __init__(self, means, stds):
-        super().__init__()
-        self.means = means
-        self.stds = stds
+        super(Normalizer, self).__init__()
+        self.register_buffer('means', means)
+        self.register_buffer('stds', stds)
     
     def forward(self, x):
-        return (x - self.means) / self.stds
+        means = self.means
+        stds = self.stds
+        return (x - means) / stds
