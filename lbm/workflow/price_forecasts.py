6674452e3733cbb81ac9fb5b97c6eca999d1b5a4
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-02-22 12:12:03 -0800

    Added price forecast workflows

---
+++ lbm/workflow/price_forecasts.py
@@ -0,0 +1,26 @@
+import torch
+
+from lbm.metrics import DirectionalAccuracy
+from lbm.metrics import ReturnError
+
+def directional_accuracy(model, dataloader,device='cpu'):
+    model.to(device)
+    model.eval()
+    accuracy = DirectionalAccuracy(device=device)
+    with torch.no_grad():
+        for inputs, targets in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            accuracy.update(inputs, targets, outputs)
+    return accuracy.compute().item()
+
+def return_error(model, dataloader,device='cpu'):
+    model.to(device)
+    model.eval()
+    accuracy = ReturnError(device=device)
+    with torch.no_grad():
+        for inputs, targets in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            accuracy.update(inputs, targets, outputs)
+    return accuracy.compute().item()
