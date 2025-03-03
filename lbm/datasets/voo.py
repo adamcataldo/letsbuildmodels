18658c7fd77e5ea1b0e9ed959db3880fde734f0b
James Cataldo <jamescataldo@Mac.attlocal.net>
2025-03-02 21:09:22 -0800

    Need to debug returns

--- lbm/datasets/voo.py
+++ lbm/datasets/voo.py
@@ -3,37 +3,12 @@ import torch
 import yfinance as yf
 from torch.utils.data import Dataset, DataLoader, Subset
 
-class Features(Enum):
-    CLOSE_PRICES = 1
-    ALL_PRICES = 2
-    ALL_PRICES_WITH_ACTIONS = 3
-
-class VOO(Dataset):
-    def __init__(self, features=Features.CLOSE_PRICES, lookback=256):
-        if features == Features.ALL_PRICES_WITH_ACTIONS:
-            d = yf.download('VOO', start='2010-09-09', auto_adjust=False, 
-                            actions=True)
-            self.x = d[['Close',
-                        'Capital Gains',
-                        'Dividends',
-                        'High',
-                        'Low',
-                        'Open',
-                        'Stock Splits',
-                        'Volume']].values
-            self.y = d['Adj Close'].values
-        elif features == Features.ALL_PRICES:
-            d = yf.download('VOO', start='2010-09-09')
-            self.x = d[['Close', 'High', 'Low', 'Open', 'Volume']].values
-            self.y = d['Close'].values
-        else:
-            d = yf.download('VOO', start='2010-09-09')
-            self.x = d['Close'].values
-            self.y = d['Close'].values
+class ClosePrices(Dataset):
+    def __init__(self, lookback=256):
+        d = yf.download('VOO', start='2010-09-09')
+        self.x = d['Close'].values
         self.x = torch.tensor(self.x, dtype=torch.float32)
-        self.y = torch.tensor(self.y, dtype=torch.float32)
-
-        self.n_points = len(self.y)
+        self.n_points = len(self.x)
         self.lookback = lookback
         
     def __len__(self):
@@ -41,12 +16,12 @@ class VOO(Dataset):
     
     def __getitem__(self, idx):
         x = self.x[idx:idx+self.lookback, :]
-        y = self.y[idx+self.lookback, 0]
+        y = self.x[idx+self.lookback, 0]
         return x, y
 
-class Preprocessor():
-    def __init__(self, features=Features.CLOSE_PRICES, lookback=256):
-        self.dataset = VOO(features, lookback)
+class ClosePricesPreprocessor():
+    def __init__(self, lookback=256):
+        self.dataset = ClosePrices(lookback)
 
         # Split NoisySine into 80/10/10, in order
         length = len(self.dataset)
@@ -65,7 +40,72 @@ class Preprocessor():
         self.test_set  = Subset(self.dataset, range(test_start, test_end))
 
     def get_loaders(self, batch_size=64, include_returns=False):
+        train_loader = DataLoader(self.train_set, 
+                                  batch_size=batch_size,
+                                  shuffle=True)
+        val_loader   = DataLoader(self.val_set,   batch_size=batch_size)
+        test_loader  = DataLoader(self.test_set,  batch_size=batch_size)
+        return train_loader, val_loader, test_loader
+    
+
+class Returns(Dataset):
+    def __init__(self, lookback=256):
+        d = yf.download('VOO', start='2010-09-09')
+        self.x = d['Close'].values
+        self.x = torch.tensor(self.x, dtype=torch.float32)
+        self.returns = self.x[1:] / self.x[:-1] - 1
+        self.n_points = len(self.returns)
+        self.lookback = lookback
+        
+    def __len__(self):
+        return self.n_points - self.lookback
+    
+    def __getitem__(self, idx):
+        x = self.returns[idx:idx+self.lookback, :]
+        y = self.returns[idx+self.lookback, 0]
+        return x, y
+
+
+class ReturnsPreprocessor():
+    def __init__(self, lookback=256):
+        self.dataset = Returns(lookback)
+        self.lookback = lookback
+
+        # Split NoisySine into 80/10/10, in order
+        length = len(self.dataset)
+        n_train = int(0.8 * length)
+        n_val   = int(0.1 * length)
+        n_test  = length - n_train - n_val
+        
+        # Indices for each split (chronological order)
+        train_start, train_end = 0, n_train
+        val_start, val_end     = train_end, train_end + n_val
+        test_start, test_end   = val_end,   val_end + n_test
+        
+        # Subset objects for train/val/test
+        self.train_set = Subset(self.dataset, range(train_start, train_end))
+        self.val_set   = Subset(self.dataset, range(val_start, val_end))
+        self.test_set  = Subset(self.dataset, range(test_start, test_end))
+        self.prices = self.dataset.x
+        self.val_offset = val_start
+        self.test_offset = test_start
+
+    def get_loaders(self, batch_size=64, include_returns=False):
         train_loader = DataLoader(self.train_set, batch_size=batch_size)
         val_loader   = DataLoader(self.val_set,   batch_size=batch_size)
         test_loader  = DataLoader(self.test_set,  batch_size=batch_size)
         return train_loader, val_loader, test_loader
+    
+    def _get_prices(self, batch_index, batch_size, count, offset):
+        start = batch_index * batch_size + offset + self.lookback - 1
+        end = start + count
+        x = self.prices[start:end, 0]
+        y = self.prices[start+1:end+1, 0]
+        return x, y
+    
+    def get_val_prices(self, batch_index, batch_size, count):
+        return self._get_prices(batch_index, batch_size, count, self.val_offset)
+    
+    def get_test_prices(self, batch_index, batch_size, count):
+        return self._get_prices(batch_index, batch_size, count, 
+                                self.test_offset)
--- tests/datasets/test_voo.py
+++ tests/datasets/test_voo.py
@@ -1,11 +1,11 @@
 import unittest
-from lbm.datasets.voo import Preprocessor, Features
+from lbm.datasets.voo import ClosePricesPreprocessor
 
 class TestPrepocessor(unittest.TestCase):
     def test_get_loaders(self):
         batch_size = 32
         lookback = 252
-        self.preprocessor = Preprocessor(lookback=lookback)
+        self.preprocessor = ClosePricesPreprocessor(lookback=lookback)
         train_loader, val_loader, test_loader = self.preprocessor.get_loaders(
             batch_size)
         for x, y in train_loader:
@@ -20,29 +20,6 @@ class TestPrepocessor(unittest.TestCase):
             self.assertEqual(x.size(), (batch_size, lookback, 1)) 
             self.assertEqual(y.size(), (batch_size,))
             break
-
-    def test_all_prices(self):
-        batch_size = 32
-        lookback = 252
-        self.preprocessor = Preprocessor(Features.ALL_PRICES, lookback=lookback)
-        train_loader, _, _ = self.preprocessor.get_loaders(batch_size)
-        for x, y in train_loader:
-            self.assertEqual(x.size(), (batch_size, lookback, 5)) 
-            self.assertEqual(y.size(), (batch_size,))
-            break
-        
-    def test_all_prices_with_actions(self):
-        batch_size = 64
-        lookback = 252
-        self.preprocessor = Preprocessor(Features.ALL_PRICES_WITH_ACTIONS,
-                                        lookback=lookback)
-        train_loader, _, _ = self.preprocessor.get_loaders(batch_size)
-        for x, y in train_loader:
-            self.assertEqual(x.size(), (batch_size, lookback, 8)) 
-            self.assertEqual(y.size(), (batch_size,))
-            break
-
-
 
 if __name__ == '__main__':
     unittest.main()
